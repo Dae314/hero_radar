9503ee44aae1333cbd6e09f53b24a40d0cfa0079
Dae314 <discord@dae314>
2022-11-16 19:05:27 -1000

    Adjust hero card styles

--- src/App.svelte
+++ src/App.svelte
@@ -16,8 +16,27 @@
 	const version = 'v1.102';
 	let curView = 'All';
 	let curType = 'f2p';
+	let curSort = 'name';
 
-	$: heroList = $HeroData.filter(hero => curView === 'All' || hero.faction === curView);
+	$: heroList = $HeroData.filter(hero => curView === 'All' || hero.faction === curView).sort((a, b) => {
+		if(curSort === 'name') {
+			const Aname = a.name.toUpperCase();
+			const Bname = b.name.toUpperCase();
+			if (Aname < Bname) {
+				return -1;
+			}
+			if (Aname > Bname) {
+				return 1;
+			}
+			return 0;
+		} else {
+			if(curType === 'f2p') {
+				return b.getF2PScore() - a.getF2PScore();
+			} else {
+				return b.getP2PScore() - a.getP2PScore();
+			}
+		}
+	});
 	$: updateType(curType);
 
 	function handleFactionChange(faction) {
@@ -54,17 +73,29 @@
 		{/each}
 	</nav>
 	<div class="optionsArea">
-		<div class="typeSwitcher">
-			<button type="button" class="typeButton" on:click={() => curType = 'f2p'}>F2P</button>
-			<button type="button" class="typeButton" on:click={() => curType = 'p2p'}>Whale</button>
+		<div class="optionSect sortSwitcher">
+			<button type="button" class="switchButton" on:click={() => curSort = 'name'}>Sort Name</button>
+			<button type="button" class="switchButton" on:click={() => curSort = 'score'}>Sort Score</button>
+		</div>
+		<div class="optionSect typeSwitcher">
+			<button type="button" class="switchButton" on:click={() => curType = 'f2p'}>F2P</button>
+			<button type="button" class="switchButton" on:click={() => curType = 'p2p'}>Whale</button>
 		</div>
 	</div>
 	<div class="titleArea">
 		{#if curType === 'f2p'}
-			<h1>Hero Usefulness for F2P</h1>
+			{#if curView === 'All'}
+				<h1>Hero Usefulness for F2P</h1>
+			{:else}
+				<h1>{curView} Usefulness for F2P</h1>
+			{/if}
 			<h2>{version}</h2>
 		{:else}
-			<h1>Hero Usefulness for Whale</h1>
+			{#if curView === 'All'}
+				<h1>Hero Usefulness for Whale</h1>
+			{:else}
+				<h1>{curView} Usefulness for Whale</h1>
+			{/if}
 			<h2>{version}</h2>
 		{/if}
 	</div>
@@ -72,14 +103,14 @@
 		{#each heroList as hero}
 			<div class="heroCard">
 				<img class="heroPortrait" src="{hero.portrait}" alt="{hero.name}">
-				<h3>{hero.name}</h3>
+				<h2>{hero.name}</h2>
 				{#if curType === 'f2p'}
-					<h3>{hero.getF2PScore()}</h3>
 					<h4>{hero.f2p_invest}</h4>
+					<h3 class="score">{hero.getF2PScore()}</h3>
 					<RadarChart data={hero.f2p_chartData} name={hero.name} />
 				{:else}
-					<h3>{hero.getP2PScore()}</h3>
 					<h4>{hero.p2p_invest}</h4>
+					<h3 class="score">{hero.getP2PScore()}</h3>
 					<RadarChart data={hero.p2p_chartData} name={hero.name} />
 				{/if}
 				<div class="notesArea">
@@ -103,6 +134,7 @@
 	main {
 		display: flex;
 		flex-direction: column;
+		padding: 10px 0px;
 	}
 
 	nav {
@@ -139,9 +171,10 @@
 	.optionsArea {
 		align-items: center;
 		display: flex;
+		flex-direction: column;
 		justify-content: center;
 		margin-bottom: 10px;
-		.typeButton {
+		.switchButton {
 			background-color: transparent;
 			border: 2px solid var(--appColorPrimary);
 			border-radius: 5px;
@@ -155,6 +188,9 @@
 				background-color: var(--appColorPrimary);
 				color: var(--appColorWhite);
 			}
+		}
+		.optionSect {
+			margin-bottom: 10px;
 		}
 	}
 
@@ -174,9 +210,9 @@
 
 	.heroDisplay {
 		display: grid;
-		grid-gap: 20px 20px;
+		grid-gap: 20px 10px;
 		grid-template-columns: repeat(auto-fill, minmax(330px, 330px));
-		grid-auto-rows: 560px;
+		grid-auto-rows: 550px;
 		justify-content: space-evenly;
 	}
 
@@ -188,20 +224,39 @@
 		flex-direction: column;
 		justify-content: center;
 		padding: 10px;
+		position: relative;
 		.heroPortrait {
 			border-radius: 50%;
 			max-width: 100px;
 		}
+		h2 {
+			margin: 0;
+		}
 		h3 {
 			margin: 3px;
+			&.score {
+				font-size: 2.0rem;
+				left: 52%;
+				margin: 0;
+				position: absolute;
+				right: 0;
+				top: 52.8%;
+				transform: translate(-50%, 50%);
+				user-select: none;
+				width: fit-content;
+			}
 		}
 		h4 {
-			margin: 3px;
+			margin: 0;
+			margin-bottom: 10px;
 		}
 		.notesArea {
 			align-items: center;
 			display: flex;
 			flex-direction: column;
+			h4 {
+				margin: 0;
+			}
 		}
 		p {
 			margin: 0px;
